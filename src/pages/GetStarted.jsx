3bf3efe36336c3c1486bc94467e8a039ace170ab
Omotoke <96818824+GirlWho-Codes@users.noreply.github.com>
2023-01-01 19:30:10 +0100

    Update GetStarted.jsx

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -1,10 +1,13 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Footer from '../components/Footer'
 import Navpage from '../components/NavPage'
+import { Select } from 'react-select';
 import emailjs from '@emailjs/browser'
 import { useNavigate } from 'react-router-dom';
+// import  {FlagIcon} from 'flag-icon-css'
+
 const GetStarted = () => {
   const form = useRef();
   const navigate = useNavigate()
@@ -17,82 +20,79 @@ const GetStarted = () => {
       .then((result) => {
           console.log(result.text);
           console.log("Sent")
-          navigate('/getfeedback');
+          navigate('/getfeedback')
       }, (error) => {
           console.log(error.text);
           alert('You have made an error!')
       });
   };
+
   return (
     <>
       <Navpage/>
-      <div className='start'>
-        <div className='start-div'>
-        <h1 className='start-head'>Get Started</h1>
+      <div className='begin'>
+        <div className='head'>
+        <h1 className='start-head'>Get Started </h1>
         </div>
-      <Form  className='start-form' onSubmit={sendEmail} ref = {form}>
-        <p className='start-p'>Let us get to know you and your business</p>
-        <div className='start-form1'>
-        <Form.Group className='border-form1' controlId="formBasicEmail">
-        <Form.Control type="text" name='user_name' placeholder="First Name"  className='border-form'/>
+        <div className='play-form'>
+
+        <p className='begin-p'>Let us get to know you and your business</p>
+      <form onSubmit={sendEmail} ref={form} className='start-form'>
+       
+        <div className='begin-form1'>
+        <Form.Group className='border-form1' controlId="formBasicName">
+        <Form.Control type="text" placeholder="First Name" name='user_name'  className='border-form'/>
         
       </Form.Group>
 
-      <Form.Group className='border-form1' controlId="formBasicEmail">
-        <Form.Control type="text" name='user_name' placeholder="Last Name"  className='border-form'/>
+      <Form.Group className='border-form1' controlId="formBasicName">
+        <Form.Control type="text" placeholder="Last Name" name='user_name' className='border-form'/>
        
       </Form.Group>
 
         </div>
-      <div className='start-form2'>
+      <div className='begin-form6'>
       <Form.Group className='border-form2' controlId="formBasicEmail">
         <Form.Control type="email" name='user_email' placeholder="Email address"  className='border-form'/>
       </Form.Group>
-      <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Phone number(Whatsapp number)"  className='border-form'/>
+      <Form.Group className='begin-form4' controlId="formBasicNumber">
+     <Form.Control 
+        type="number" placeholder="Phone number(Whatsapp number preferably)"  className='border-form6'/>
       </Form.Group>
-
-      <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" name='message' placeholder="Address"  className='border-form'/>      
-        </Form.Group>
       </div>
 
-        <div className='start-form1'>
-        <Form.Group className='border-form1' controlId="formBasicEmail">
-       
-        <Form.Control type="text" placeholder="City" name='message'  className='border-form'/>
-      </Form.Group>
-      <Form.Group className='border-form1' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Country" name='message' className='border-form'/>
+        <div className='begin-form6'>
+        <Form.Group className='begin-form5' controlId="formBasicText">
+        <Form.Control type="text" name='message' placeholder="How did you hear about us"  className='border-form'/>      
+        </Form.Group>
+        
+        </div>
+        <div className='begin-form6'>
+        <Form.Group className='begin-form8' controlId="formBasicText">
+        <Form.Control type="text" name='message' placeholder="Address"  className='border-form'/>      
+        </Form.Group>
+        
+        </div>
+
+        <div className='start-form3'>
+        <Form.Group className='border-form1' controlId="formBasicName">
+        <Form.Control type="text" placeholder="Country" name='user_name'  className='border-form'/>
         
       </Form.Group>
+
+      <Form.Group className='border-form1' controlId="formBasicName">
+        <Form.Control type="text" placeholder="City" name='user_name' className='border-form'/>
+       
+      </Form.Group>
+
         </div>
-
-        <div className='start-form2'>
-        <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Company name" name='message' className='border-form'/>      
-        </Form.Group>
-
-        <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Company url(optional)" name='message' className='border-form'/>      
-        </Form.Group>
-
-        <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="number" name='message' placeholder="Number of employees"  className='border-form'/>      
-        </Form.Group>
-
-        <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="Areas of concern" name='message' className='border-form'/>      
-        </Form.Group>
-
-        <Form.Group className='border-form2' controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="How did you hear about us" name='message'  className='border-form'/>      
-        </Form.Group>
-        </div>
-      <Button variant="primary" className='border-btn'  type="submit">
+       
+      <Button variant="primary" className='begin-btn' type="submit">
         Submit
       </Button>
-    </Form>
+    </form>
+
+        </div>
       </div>
       <Footer/>
     </>
